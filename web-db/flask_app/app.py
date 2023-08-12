740439d08ee754b842c82f06ff18d0ba2015f380
MARCELO CONDORI APALA <cam5000810@est.univalle.edu>
2023-08-11 22:36:28 -0400

    Complete practice

--- web-db/flask_app/app.py
+++ web-db/flask_app/app.py
@@ -1,7 +1,8 @@
-from flask import Flask,redirect,render_template, request, jsonify,url_for
+from flask import Flask,redirect,render_template, request, jsonify,url_for,flash
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import json
+import logging
 
 app = Flask(__name__)
 
@@ -10,6 +11,8 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Univalle'
 app.config['MYSQL_DB'] = 'example'
 app.config['MYSQL_PORT'] = 3306
+
+app.secret_key="mysecretkey"
 
 mysql = MySQL(app)
 
@@ -46,20 +49,39 @@ def student_create_json():
         return redirect(url_for('student_list'))
     return render_template('create.html')
             
-@app.route('/update_student/<int:student_id>', methods=['PUT'])
-def update_student(student_id):
-    if request.method == 'PUT':
+@app.route('/studentupdate/<string:id>',methods=['GET'])
+def student_update(id):
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute("SELECT id,first_name,last_name,city,semester FROM student WHERE id =%s",(id))
+    data=cur.fetchall()
+    print(data)
+    logging.debug(data)
+    return render_template('update.html',student=data[0])
+
+@app.route('/studentupdate/<string:id>', methods=['POST'])
+def student_update_json(id):
+    if request.method == 'POST':
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         city = request.form['city']
         semester = request.form['semester']
-
-        cursor = mysql.connection.cursor()
-        cursor.execute('UPDATE student SET first_name=%s, last_name=%s, city=%s, semester=%s WHERE id=%s',
-                       (first_name, last_name, city, semester, student_id))
+        cur = mysql.connection.cursor()
+        cur.execute('UPDATE student SET first_name=%s, last_name=%s, city=%s, semester=%s WHERE id=%s',
+                       (first_name, last_name, city, semester, id))
         mysql.connection.commit()
+        cur.close()
+        flash('successfully updated')
+        return redirect(url_for('student_list'))
+    return render_template('update.html')
 
-        return jsonify(message='Student updated successfully')
+@app.route('/studentdelete/<string:id>')
+def student_delet(id):
+    cur=mysql.connection.cursor()
+    cur.execute('DELETE FROM student WHERE id=%s',(id))
+    mysql.connection.commit()
+    cur.close()
+    flash('successfully removed')
+    return redirect(url_for('student_list'))
         
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81, debug=True)
